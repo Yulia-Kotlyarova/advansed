e6ef00701e6f51a3f055637fcce08ec9f9cda3e0
julia.kotlyarova <julia.kotlyarova@axamit.com>
2023-02-27 11:18:00 +0300

    develop fix: delete empty style file

--- src/features/AuthByUserName/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUserName/ui/LoginModal/LoginModal.tsx
@@ -1,24 +1,19 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from 'features/AuthByUserName/ui/LoginForm/LoginForm';
-import { useEffect, useState } from 'react';
-import classes from './LoginModal.module.scss';
 
 interface LoginModalProps {
-    className?: string;
     isOpen: boolean,
     onClose?: () => void;
     lazy?: boolean;
 }
 
 export const LoginModal = ({
-    className, isOpen, lazy, onClose,
+    isOpen, lazy, onClose,
 }: LoginModalProps) => (
     <Modal
         isOpen={isOpen}
         onClose={onClose}
         lazy={lazy}
-        className={classNames(classes.LoginModal, {}, [className])}
     >
         <LoginForm />
     </Modal>
