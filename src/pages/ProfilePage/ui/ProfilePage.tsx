3c22a8fd048e52bd0ab4faba5c9f203aec978b65
julia.kotlyarova <julia.kotlyarova@axamit.com>
2023-03-12 17:45:32 +0300

    develop fix: profile import

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from 'react-i18next';
 import { BaseText } from 'shared/ui/BaseText/BaseText';
 import { DynamicModalLoader, ReducersList } from 'shared/lib/components/DynamicModalLoader/DynamicModalLoader';
 import { profileReducer } from 'entities/Profile';
-import classes from './ProfilePage.module.scss';
 
 interface ProfilePageProps {
   className?: string;
@@ -17,7 +16,7 @@ const ProfilePage = ({ className }: ProfilePageProps) => {
     const { t } = useTranslation('profile');
     return (
         <DynamicModalLoader reducers={reducers}>
-            <div className={classNames(classes.ProfilePage, {}, [className])}>
+            <div className={classNames('', {}, [className])}>
                 <BaseText title={t('profile page')} />
             </div>
         </DynamicModalLoader>
